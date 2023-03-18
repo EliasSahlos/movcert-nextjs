4c0097d72d2528faf67f2f0cce00b7118967a1ea
EliasSahlos <esahlos12@gmail.com>
2023-03-18 16:54:28 +0200

    Started working on concerts page. Filter Card component made for concerts

---
+++ components/FilterCard.js
@@ -0,0 +1,134 @@
+import { useEffect, useState } from "react";
+
+function FilterCard() {
+    const [screenWidth, setScreenWidth] = useState(0);
+
+    useEffect(() => {
+        function handleResize() {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [setScreenWidth]);
+
+
+    return (
+        <>
+            {screenWidth < 768 ? (
+                <div className="flex justify-center items-center">
+                    <div className=" block w-[550px] p-8 mt-[150px] border rounded-lg shadow-md bg-white z-10 md:grid grid-cols-4 gap-4 w-[800px]">
+                        <p className="mb-2 text-[16px] font-bold text-black">Concert Title</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className=" border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <p className="mb-2 text-[16px] font-bold text-black">Music Genre</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className=" border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <p className="mb-2 text-[16px] font-bold text-black">Price - Low</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className=" border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <p className="mb-2 text-[16px] font-bold text-black">Price - High</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className=" border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <div className="flex justify-center items-center mt-4">
+                            <button className=" bg-[#ffba5a] text-white w-[120px] h-[50px] rounded-full ">Set Filters </button>
+                        </div>
+                    </div>
+                </div>
+            ) : screenWidth < 1024 ? (
+                <div className="flex justify-center items-center">
+                    <div className=" block w-[550px] p-8 mt-[150px] border rounded-lg shadow-md bg-white z-10 md:grid grid-cols-4 gap-4 w-[800px]">
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Concert Title</p>
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Music Genre</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Price - Low</p>
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Price - High</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <div className="relative ml-[280px] mt-4 ">
+                            <button className=" bg-[#ffba5a] text-white w-[140px] h-[50px] rounded-full ">Set Filters </button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className="flex justify-center items-center">
+                    <div className=" block w-[1000px] p-8 mt-[150px] border rounded-lg shadow-md bg-white z-10 md:grid grid-cols-8 gap-4 w-[800px]">
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Concert Title</p>
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Music Genre</p>
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Price - Low</p>
+                        <p className="col-span-2 mb-2 text-[16px] font-bold text-black">Price - High</p>
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <input
+                            type="text"
+                            name="Search"
+                            id="cTitle"
+                            className="col-span-2 border-2 border-[##ced4da] h-[50px] p-2 w-full outline-none rounded shadow-sm focus:border-[#f3c07a] "
+                        />
+                        <div className="relative ml-[380px] mt-4 ">
+                            <button className=" bg-[#ffba5a] text-white w-[140px] h-[50px] rounded-full">Set Filters </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default FilterCard;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import HeaderBar from "@/components/HeaderBar";
 import "@/styles/globals.css";
+import "../styles/mouse-scroll-icon.css"
 
 export default function App({ Component, pageProps }) {
     return (
         <>
-           
                 <Component {...pageProps} />
-            
         </>
     );
 }
