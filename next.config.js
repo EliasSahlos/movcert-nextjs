51ce8d701b2f39711b8dd7b0853a1dd6c5b62a88
EliasSahlos <esahlos12@gmail.com>
2023-03-25 18:17:00 +0200

    fixed domains config for images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
 module.exports = {
     images: {
-        domains: ['static.wikia.nocookie.net']['i.pinimg.com'],
-    }
-}
+        domains: ["static.wikia.nocookie.net","i.pinimg.com","randoms.lv"]
+    },
+};
