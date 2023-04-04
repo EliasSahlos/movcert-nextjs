e3a98d8c62bcc0ab010d3e8697d4cf308dfadd9c
AlexaderLoco <alexlocopap@gmail.com>
2023-04-04 18:25:09 +0300

    Slight margin fix on the title.

--- components/ConcertCard.js
+++ components/ConcertCard.js
@@ -20,7 +20,7 @@ function ConcertCard({ screenWidth, concertsData }) {
                                 alt="broken-img"
                                 className="rounded h-auto max-w-full shadow-xl scale-100 hover:scale-105 ease-in duration-100"
                             />
-                            <h1 className="text-center mt-2">{concert.title}</h1>
+                            <h1 className="text-center mt-4">{concert.title}</h1>
                             </Link>
                         </div>
                     </div>
