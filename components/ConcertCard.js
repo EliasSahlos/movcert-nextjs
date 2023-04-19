d02286228d23cb091c7690fcb698406568c2e986
EliasSahlos <esahlos12@gmail.com>
2023-04-19 13:05:09 +0300

    Minor bug fixes

--- components/ConcertCard.js
+++ components/ConcertCard.js
@@ -8,10 +8,10 @@ function ConcertCard({ screenWidth, concertsData }) {
     
     return (
         <>
-            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4" data-aos="fade-up">
                 {concertsData?.map((concert) => (
-                    <div key={concert.id}>
-                        <div className="mb-2">
+                    <div key={concert.id} data-aos="fade-up">
+                        <div className="mb-2" data-aos="fade-up">
                             <Link href={'/concerts/' + concert.id}>
                             <Image
                                 src={concert.concertCover}
@@ -20,7 +20,7 @@ function ConcertCard({ screenWidth, concertsData }) {
                                 alt="broken-img"
                                 className="rounded h-auto max-w-full shadow-xl scale-100 hover:scale-105 ease-in duration-100"
                             />
-                            <h1 className="text-center mt-4">{concert.title}</h1>
+                            <h1 className="text-center mt-4" data-aos="fade-up">{concert.title}</h1>
                             </Link>
                         </div>
                     </div>
