1a8822dd65cfd84643f0e1921f9867931e410216
EliasSahlos <esahlos12@gmail.com>
2023-04-19 15:49:10 +0300

    Fixed minor bugs.

--- pages/concerts/index.js
+++ pages/concerts/index.js
@@ -2,20 +2,20 @@ import ConcertCard from "@/components/ConcertCard";
 import FilteredConcertCard from "@/components/FilteredConcertCard";
 import FilterCard from "@/components/FilterCard";
 import Head from "next/head";
-import { collection, doc, getDocs, onSnapshot, query, where } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "../../firebase/firebase";
-import { UserAuth } from "@/context/AuthContext";
+import {collection, getDocs, query, where} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {db} from "../../firebase/firebase";
+import {UserAuth} from "@/context/AuthContext";
 
-function Concerts({ screenWidth }) {
+function Concerts({screenWidth}) {
     const [concerts, setConcerts] = useState([]);
     const [filteredConcerts, setFilteredConcerts] = useState([]);
-    const { user } = UserAuth();
+    const {user} = UserAuth();
 
     useEffect(() => {
         async function getConcertData() {
             const concertsData = await getDocs(collection(db, "concerts"));
-            setConcerts(concertsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            setConcerts(concertsData.docs.map((doc) => ({...doc.data(), id: doc.id})));
         }
 
         getConcertData();
@@ -26,7 +26,7 @@ function Concerts({ screenWidth }) {
         const genre = concertFilter.cGenre;
         const cPriceLowNumber = parseInt(concertFilter.cPriceLow);
         const cPriceHighNumber = parseInt(concertFilter.cPriceHigh);
-
+        const newFilteredConcerts = [];
         try {
             const concertRef = collection(db, "concerts");
             if (genre.length !== 0 && genre !== "Any" && !isNaN(cPriceLowNumber) && !isNaN(cPriceHighNumber)) {
@@ -39,19 +39,29 @@ function Concerts({ screenWidth }) {
                     where("price", "<=", cPriceHighNumber)
                 );
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             } else if (genre === "Any" && !isNaN(cPriceLowNumber) && !isNaN(cPriceHighNumber)) {
                 console.log("ALL FILTERS (ANY)");
 
                 const q = query(concertRef, where("price", ">=", cPriceLowNumber), where("price", "<=", cPriceHighNumber));
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             } else if (genre.length !== 0 && genre !== "Any" && isNaN(cPriceLowNumber) && isNaN(cPriceHighNumber)) {
                 console.log("ONLY GENRE");
 
                 const q = query(concertRef, where("genre", "==", genre));
                 const querySnapshot = await getDocs(q);
-                const newFilteredConcerts = [];
+
                 querySnapshot.forEach((doc) => {
                     let data = doc.data();
                     data.id = doc.id;
@@ -68,25 +78,45 @@ function Concerts({ screenWidth }) {
 
                 const q = query(concertRef, where("genre", "==", genre), where("price", ">=", cPriceLowNumber));
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             } else if (genre === "Any" && !isNaN(cPriceLowNumber) && isNaN(cPriceHighNumber)) {
                 console.log("GENRE AND LOWPRICE (ANY)");
 
                 const q = query(concertRef, where("price", ">=", cPriceLowNumber));
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             } else if (genre.length !== 0 && genre !== "Any" && isNaN(cPriceLowNumber) && !isNaN(cPriceHighNumber)) {
                 console.log("GENRE AND HIGHPRICE");
 
                 const q = query(concertRef, where("genre", "==", genre), where("price", "<=", cPriceHighNumber));
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             } else if (genre === "Any" && isNaN(cPriceLowNumber) && !isNaN(cPriceHighNumber)) {
                 console.log("GENRE AND HIGHPRICE (ANY)");
 
                 const q = query(concertRef, where("price", "<=", cPriceHighNumber));
                 const querySnapshot = await getDocs(q);
-                querySnapshot.forEach((doc) => {});
+                querySnapshot.forEach((doc) => {
+                    let data = doc.data();
+                    data.id = doc.id;
+                    newFilteredConcerts.push(data);
+                });
+                setFilteredConcerts(newFilteredConcerts)
             }
         } catch (error) {
             console.log(error);
@@ -97,15 +127,15 @@ function Concerts({ screenWidth }) {
         <>
             <Head>
                 <title>Movcert - Concerts</title>
-                <meta name="description" content="Generated by create next app" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="Generated by create next app"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
 
             <div className="h-[700px] bg-cover bg-fixed custom-img " data-aos="fade">
                 <div className="flex items-center justify-center">
-                    <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-black h-[140px]" />
-                    <div className="absolute h-[700px] top-0 bottom-0 right-0 left-0 bg-black/40 z-0 " />
+                    <div className="absolute top-0 bottom-0 right-0 left-0 bg-gradient-to-b from-black h-[140px]"/>
+                    <div className="absolute h-[700px] top-0 bottom-0 right-0 left-0 bg-black/40 z-0 "/>
                     <div className="z-10 mt-[320px]">
                         <h1 className="font-bold text-center text-[50px] text-white mt-[-85px]" data-aos="fade-up">
                             Concerts
@@ -118,14 +148,15 @@ function Concerts({ screenWidth }) {
                     </div>
                 </div>
                 <div data-aos="fade-up" data-aos-offset="200" data-aos-once="false">
-                    <FilterCard screenWidth={screenWidth} onGetConcertFilterData={getConcertFilterData} />
+                    <FilterCard screenWidth={screenWidth} onGetConcertFilterData={getConcertFilterData}/>
                 </div>
 
-                <div className="mt-8 p-4 flex justify-center items-center" data-aos="fade-up" data-aos-offset="200" data-aos-once="false">
+                <div className="mt-8 p-4 flex justify-center items-center" data-aos="fade-up" data-aos-offset="200"
+                     data-aos-once="false">
                     {filteredConcerts.length === 0 ? (
-                        <ConcertCard screenWidth={screenWidth} concertsData={concerts} />
+                        <ConcertCard screenWidth={screenWidth} concertsData={concerts}/>
                     ) : (
-                        <FilteredConcertCard screenWidth={screenWidth} concertsData={filteredConcerts} />
+                        <FilteredConcertCard screenWidth={screenWidth} concertsData={filteredConcerts}/>
                     )}
                 </div>
             </div>
